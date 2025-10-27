b5bc2fb13eaf91b133bf4ed660c7052485a6c676
Alexandre Savaris <alexandre.savaris@gmail.com>
2025-10-27 19:33:25 -0300

    StreamsOfGenerateIterateExample

---
+++ java-8/src/com/learnJava/streams/StreamsOfGenerateIterateExample.java
@@ -0,0 +1,26 @@
+package com.learnJava.streams;
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamsOfGenerateIterateExample {
+
+    public static void main(String[] args) {
+
+        Stream<String> stringStream = Stream.of("adam", "dan", "julie");
+        stringStream.forEach(System.out::println);
+
+        System.out.println("===================================================");
+
+        Stream.iterate(1, x -> x * 2)
+            .limit(50)
+            .forEach(System.out::println);
+
+        System.out.println("===================================================");
+
+        Supplier<Integer> supplier = new Random()::nextInt;
+        Stream.generate(supplier)
+            .forEach(System.out::println);
+    }
+}
